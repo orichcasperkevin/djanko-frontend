110fef6c1c7f78c8e411e5ed6fa8a1348ea2352f
orichcasperkevin <orichcaspertheghost@gmail.com>
2023-10-16 16:06:30 +0300

    first commit

---
+++ src/components/HankoAuth.js
@@ -0,0 +1,31 @@
+import { useEffect, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { register, Hanko } from "@teamhanko/hanko-elements";
+import { useMemo } from 'react';
+
+const hankoApi = process.env.REACT_APP_HANKO_API_URL;
+
+export default function HankoAuth() {
+  const navigate = useNavigate();
+  const hanko = useMemo(() => new Hanko(hankoApi), []);
+
+  const redirectAfterLogin = useCallback(() => {
+    navigate("/dashboard");
+  }, [navigate]);
+
+  useEffect(
+    () =>
+      hanko.onAuthFlowCompleted(() => {
+        redirectAfterLogin();
+      }),
+    [hanko, redirectAfterLogin]
+  );
+
+  useEffect(() => {
+    register(hankoApi).catch((error) => {
+      // handle error
+    });
+  }, []);
+
+  return <hanko-auth />;
+}
